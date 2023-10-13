b098156612882d8b8475bebeb0194666c73098df
DawidZ <dawidziobrowski.dz@gmail.com>
2023-10-13 18:38:29 +0200

    Fixed stupid links

--- src/components/atoms/navbar/active-link.tsx
+++ src/components/atoms/navbar/active-link.tsx
@@ -2,30 +2,30 @@
 
 import Link from "next/link";
 import { type Route } from "next";
+import React from "react";
+import { usePathname } from "next/navigation";
 
 const ActiveLink = <T extends string>({
   href,
-  text,
+  children,
   className = "rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white",
   activeClassName = "rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white",
-  isActive,
-  onClick,
 }: {
   href: Route<T> | URL;
-  text: string;
+  children: React.ReactNode;
   className?: string;
   activeClassName?: string;
-  isActive: boolean;
-  onClick?: () => void;
 }) => {
+  const pathname = usePathname();
+  const [isActive, setIsActive] = React.useState(false);
+
+  React.useEffect(() => {
+    setIsActive(href === pathname);
+  }, [pathname]);
+
   return (
-    <Link
-      href={href}
-      className={isActive ? activeClassName : className}
-      aria-current={isActive}
-      onClick={onClick}
-    >
-      {text}
+    <Link href={href} className={isActive ? activeClassName : className} aria-current={isActive}>
+      {children}
     </Link>
   );
 };
