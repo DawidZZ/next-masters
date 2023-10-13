30630e246ded3fa7f04f530b331030cdbcb81a19
DawidZ <dawidziobrowski.dz@gmail.com>
2023-10-13 22:25:45 +0200

    Added exact to activelink

--- src/components/atoms/navbar/active-link.tsx
+++ src/components/atoms/navbar/active-link.tsx
@@ -10,14 +10,18 @@ const ActiveLink = <T extends string>({
   children,
   className = "rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white",
   activeClassName = "rounded-md bg-gray-900 px-4 py-2 text-sm font-medium text-white",
+  exact = false,
 }: {
   href: Route<T> | URL;
   children: React.ReactNode;
   className?: string;
   activeClassName?: string;
+  exact?: boolean;
 }) => {
   const pathname = usePathname();
-  const isActive = pathname === href;
+  const generalPathname = `/${pathname.split("/")[1]}`;
+
+  const isActive = exact ? pathname === href : generalPathname === href;
 
   return (
     <>
