ff5e676b5f9224ccde60015c1de01973718a0dc4
DawidZ <dawidziobrowski.dz@gmail.com>
2023-10-14 19:17:18 +0200

    change loading background

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,7 +2,7 @@ import Spinner from "@/components/atoms/shared/spinner";
 
 export default function Loading() {
   return (
-    <div aria-busy className="flex h-full w-full items-center justify-center">
+    <div aria-busy className="flex h-full w-full items-center justify-center bg-white">
       <Spinner color="#6366f1" />
     </div>
   );
