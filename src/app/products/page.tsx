998b18867c41e6c6872205d7d161bdc80a4e2f87
DawidZ <dawidziobrowski.dz@gmail.com>
2023-10-14 16:54:08 +0200

    Added redirect

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,14 +1,5 @@
-import Pagination from "@/components/organisms/pagination";
-import ProductsGrid from "@/components/organisms/products-grid";
+import { redirect } from "next/navigation";
 
 export default async function Products() {
-  const response = await fetch("https://naszsklep-api.vercel.app/api/products?take=20");
-  const products = (await response.json()) as Product[];
-
-  return (
-    <div className="min-h-full bg-white">
-      <ProductsGrid products={products} />
-      <Pagination from={1} to={10} href="/products" />
-    </div>
-  );
+  return redirect("/products/1");
 }
